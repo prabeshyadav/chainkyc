f06acd0ccc2c5b472c00a700575145ae34b3420c
prabeshyadav <prabeshyadav087@gmail.com>
2026-07-29 01:49:12 +0545

    image is visible now

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,3 +16,9 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT,
     )
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
+
